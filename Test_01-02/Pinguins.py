9c74d5c14d58295668c5a1a7bca96aa4ef2b8233
M7ionov <m7ionov@ukr.net>
2021-05-05 00:52:20 +0300

    Test 01 02

---
+++ Test_01-02/Pinguins.py
@@ -0,0 +1,13 @@
+quantity = int(input())
+
+ping1 = '   _~_   '
+ping2 = '  (o o)  '
+ping3 = ' /  V  \\ '
+ping4 = '/(  _  )\\'
+ping5 = '  ^^ ^^  '
+
+print((ping1 + ' ') * quantity)
+print((ping2 + ' ') * quantity)
+print((ping3 + ' ') * quantity)
+print((ping4 + ' ') * quantity)
+print((ping5 + ' ') * quantity)
