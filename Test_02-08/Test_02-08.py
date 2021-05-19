46c6e2123d8ce9ac2ea1ea477b2f599bc53a08de
M7ionov <m7ionov@ukr.net>
2021-05-19 23:43:40 +0300

    Шоколадка.

--- Test_02-08/Test_02-08.py
+++ Test_02-08/Test_02-08.py
@@ -1,7 +1,7 @@
 n = int(input())
 m = int(input())
 k = int(input())
-if (k % n) == 0 or (k % m) == 0 and (n * m >= k):
-    print("YES")
-else:
+if (k % n) != 0 and ((k % m) != 0 or m < k):
     print("NO")
+else:
+    print("YES")
