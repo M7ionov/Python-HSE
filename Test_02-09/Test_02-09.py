f8745d4fd31e4aa2142f2fbe88109d815e47b0ac
M7ionov <m7ionov@ukr.net>
2021-05-20 00:28:42 +0300

    Коровы.

--- Test_02-09/Test_02-09.py
+++ Test_02-09/Test_02-09.py
@@ -1,7 +1,7 @@
 n = int(input())
 if (n % 10 == 1):
-    print(n,"korova")
-elif ((10 < n < 20) or (n % 10 == 0) or (n % 10 == 5) or (n % 10 == 6) or (n % 10 == 7) or (n % 10 == 8) or (n % 10 == 9)):
-    print(n,"korov")
+    print(n, "korova")
+elif ((10 < n < 20) or (n % 10 == (0 or 5 or 6 or 7 or 8 or 9))):
+    print(n, "korov")
 else:
-    print(n,"korovy")
+    print(n, "korovy")
