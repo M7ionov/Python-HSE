c941b70e9c5a888b0d30938787c571f476cc13a5
M7ionov <m7ionov@ukr.net>
2021-05-21 02:06:16 +0300

    Тип треугольника.

--- Test_02-13/Test_02-13.py
+++ Test_02-13/Test_02-13.py
@@ -2,28 +2,11 @@ a = int(input())
 b = int(input())
 c = int(input())
 if a + b > c or b + c > a or a + c > b:
-    if a > b and a > c:
-        if a ** 2 == b ** 2 + c ** 2:
-            print("rectangular")
-        elif a ** 2 < b ** 2 + c ** 2:
-            print("acute")
-        elif a ** 2 > b ** 2 + c ** 2:
-            print("obtuse")
-    if b > a and b > c:
-        if b ** 2 == a ** 2 + c ** 2:
-            print("rectangular")
-        elif b ** 2 < a ** 2 + c ** 2:
-            print("acute")
-        elif b ** 2 > a ** 2 + c ** 2:
-            print("obtuse")
-    if c > a and c > b:
-        if c ** 2 == a ** 2 + b ** 2:
-            print("rectangular")
-        elif c ** 2 < a ** 2 + b ** 2:
-            print("acute")
-        elif c ** 2 > a ** 2 + b ** 2:
-            print("obtuse")
-    if a == b and a == c:
+    if (a * a + b * b == c * c) or (a * a + c * c == b * b) or (b * b + c * c == a * a):
+        print("rectangular")
+    elif (a * a + b * b < c * c) or (a * a + c * c < b * b) or (c * c + b * b < a * a):
+        print("obtuse")
+    else:
         print("acute")
 else:
     print("impossible")
