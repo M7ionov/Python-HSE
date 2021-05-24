2b2cf7eeb5cccca32f293f0e160989ace6ced11e
M7ionov <m7ionov@ukr.net>
2021-05-25 02:36:02 +0300

    Площадь треугольника.

--- Test_03-01/Test_03-01.py
+++ Test_03-01/Test_03-01.py
@@ -1,6 +1,6 @@
-a = int(input())
-b = int(input())
-c = int(input())
+a = float(input())
+b = float(input())
+c = float(input())
 p = (a + b + c) / 2
 s = (p * (p - a) * (p - b) * (p - c)) ** (1/2)
 print('{:.6f}'.format(s))
