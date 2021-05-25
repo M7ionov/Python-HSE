ddf196ff0966b63d225051a56c6613f1a9f84b05
M7ionov <m7ionov@ukr.net>
2021-05-25 03:14:24 +0300

    Четные и нечетные.

--- Test_02-14/Test_02-14.py
+++ Test_02-14/Test_02-14.py
@@ -1,7 +1,10 @@
 a = int(input())
 b = int(input())
 c = int(input())
-if (a % 2 == 0 or b % 2 == 0 or c % 2 == 0) and (a % 2 == 1 or b % 2 == 1 or c % 2 == 1):
+a2 = a % 2
+b2 = b % 2
+c2 = c % 2
+if (a2 == 0 or b2 == 0 or c2 == 0) and (a2 == 1 or b2 == 1 or c2 == 1):
     print("YES")
 else:
     print("NO")
