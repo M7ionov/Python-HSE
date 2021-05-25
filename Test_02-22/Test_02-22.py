cf00f4591871c9b273874608a2c6a905672cf475
M7ionov <m7ionov@ukr.net>
2021-05-26 00:36:30 +0300

    Котлеты.

--- Test_02-22/Test_02-22.py
+++ Test_02-22/Test_02-22.py
@@ -1,7 +1,7 @@
 k, m, n = int(input()), int(input()), int(input())
 if n <= k:
     print(2 * m)
-elif n % k == 0:
+elif (n % k) == 0:
     print(2 * m * n // k)
 else:
     print(2 * m * (1 + n // k))
