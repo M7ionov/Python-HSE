62cf5d87276bd928f9492a1702b588fee3d4dded
M7ionov <m7ionov@ukr.net>
2021-05-28 00:17:35 +0300

    Спички.

--- Test_02-23/Test_02-23.py
+++ Test_02-23/Test_02-23.py
@@ -1,3 +1,25 @@
 l1, r1 = int(input()), int(input())
 l2, r2 = int(input()), int(input())
 l3, r3 = int(input()), int(input())
+x = l1 <= r2 and l2 <= r1
+y = l2 <= r3 and l3 <= r2
+z = l1 <= r3 and l3 <= r1
+length1 = r1 - l1
+length2 = r2 - l2
+length3 = r3 - l3
+if x == y is True or x == z is True or y == z is True:
+    print(0)
+elif l2 <= r3 and l3 <= r2:
+    print(1)
+elif r2 <= l3 <= length1 + r2 or r3 <= l2 <= length1 + r3:
+    print(1)
+elif l1 <= r3 and l3 <= r1:
+    print(2)
+elif r1 <= l3 <= length2 + r1 or r3 <= l1 <= length2 +r3:
+    print(2)
+elif l2 <= r1 and l1 <= r2:
+    print(3)
+elif r1 <= l2 <= length3 + r1 or r2 <= l1 <= length3 + r2:
+    print(3)
+else:
+    print(-1)
