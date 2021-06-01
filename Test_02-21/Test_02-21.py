705c266c10aeae1b1c140298fe24d312354928d1
M7ionov <m7ionov@ukr.net>
2021-06-01 22:37:44 +0300

    Сложное уравнение.

--- Test_02-21/Test_02-21.py
+++ Test_02-21/Test_02-21.py
@@ -1,9 +1,7 @@
 a, b, c, d = int(input()), int(input()), int(input()), int(input())
-if a == 0 and b == 0:
+if a == 0:
     print("INF")
-elif a == 0 or b / a == d / c or (c and d) == 0:
+elif (b % a ) != 0 or (c * int(-b / a) + d == 0):
     print("NO")
-elif b % a == 0:
-    print(-b // a)
 else:
-    print("NO")
+    print(-b // a)
