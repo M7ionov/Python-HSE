186c2b01525bae9da0fd02114ee6aaf78c95a656
M7ionov <m7ionov@ukr.net>
2021-06-20 02:27:46 +0300

    Количество палиндромов.

--- Test_02-44/Test_02-44.py
+++ Test_02-44/Test_02-44.py
@@ -1,1 +1,14 @@
 k = int(input())
+rev = 0
+copy = k
+q = 0
+while k != 0:
+    while copy > 0:
+        rev = rev * 10 + copy % 10
+        copy //= 10
+    if rev == k:
+        q += 1
+    rev = 0
+    k -= 1
+    copy = k
+print(q)
