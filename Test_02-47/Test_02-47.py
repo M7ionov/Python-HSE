30c252d1c533af826fe65e376750c66b352fea5a
M7ionov <m7ionov@ukr.net>
2021-06-20 05:33:56 +0300

    Наименьшее расстояние между локальными максимумами.

--- Test_02-47/Test_02-47.py
+++ Test_02-47/Test_02-47.py
@@ -1,21 +1,21 @@
 a1, a2, a3 = int(input()), int(input()), int(input())
-i, count, min = 3, 0, 0
-while a3 !=0:
+i, count, mmin = 3, 0, 0
+while a3 != 0:
     if a2 > a1 and a2 > a3:
         count += 1
         if count == 1:
-            max = i
+            mmax = i
         elif count == 2:
-            s = i - max
-            min = s
-            max = i
+            s = i - mmax
+            mmin = s
+            mmax = i
         elif count > 2:
-            s = i - max
-            if s < min:
-                min = s
-            max = i
+            s = i - mmax
+            if s < mmin:
+                mmin = s
+            mmax = i
     a1 = a2
     a2 = a3
     a3 = int(input())
     i += 1
-print(min)
+print(mmin)
