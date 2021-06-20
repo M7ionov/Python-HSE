5b915171b66de6a9f59abacb5f50bc78b63226c4
M7ionov <m7ionov@ukr.net>
2021-06-20 03:33:15 +0300

    Максимальное число подряд идущих равных.

--- Test_02-45/Test_02-45.py
+++ Test_02-45/Test_02-45.py
@@ -1,1 +1,13 @@
 n = int(input())
+copy = n
+i1, i2 = 0, 0
+while n != 0:
+    if n == copy:
+        i2 += 1
+        if i2 > i1:
+            i1 = i2
+    else:
+        copy = n
+        i2 = 1
+    n = int(input())
+print(i1)
